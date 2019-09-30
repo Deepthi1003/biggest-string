14d90c92c51d9d90cecb44989f787967ea87a8b9
Deepthi1003 <dtchokka@gmail.com>
2019-09-30 10:31:34 -0500

    including 4th string for comparisions

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-function myfunction() {
+function comparisions() {
   let a = document.getElementById("firststring").value;
   let b = document.getElementById("secondstring").value;
   let c = document.getElementById("thirdstring").value;
+  let d = document.getElementById("fourthstring").value;
   let name = document.getElementById("guest").value
   let biggest = '';
-  if (a.length > b.length && a.length > c.length) {
+  if (a.length > b.length && a.length > c.length && a.length > d.length) {
     biggest += a;
-  } else if (b.length > c.length && b.length > a.length) {
+  } else if (b.length > c.length && b.length > a.length && b.lenght > d.length) {
     biggest += b;
-  } else {
-    biggest += c;
+  } else if(c.length > a.length && c.length > b.length && c.length > d.length) {
+    {
+      biggest +=c;
+    }
+    else{
+    biggest += d;
   }
-  document.getElementById("result").innerHTML = name+" , the biggest String among three is " +biggest;
+  
+  document.getElementById("result").innerHTML = name+" , the biggest String among four is " +biggest;
 }
